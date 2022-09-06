e53176cd2902a63501fed3ba32149aea903f66ca
Michael2004007 <112979192+Michael2004007@users.noreply.github.com>
2022-09-06 15:08:16 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+const yesBtn = document.querySelector('#yesBtn');
+
+yesBtn.addEventListener('click',function () {
+    alert('Amor dale a Aceptar♥️♥️♥️')
+     alert('Amor ya se que ahora ya sos mi novia y tengo guardado demas bien la fecha en el que me dijiste que si😻♥️😻♥️ ')
+     alert('Apuesto a que no sabes la fecha que me dijiste que si amor o sabes hina amor ??? 😂jajaja')
+     alert('Amor yo se que estas dormiendo jajaja y mientras dormias quise Hacer algo para vos amor')
+     alert('Te Quiero demas Amor😻♥️')
+     alert('Amor No es gran cosa jaja pero si me llevo muchisimo tiempo hacer amor jajaja')
+     alert('Mi amor te elijo a ti una y mil veces, por encima de quien sea, cuando sea y donde sea, porque simplemente quiero que seas tú la persona con la que pueda pasar el resto de mi vida, con quien crear toda una vida juntos, quiero que seas la persona a la que tenga que ver todos los días a mi lado cuando me levante, que seas la persona la cual algun dia poder formar una familia y casarnos, y que me dejes ser quien te acompañe en tus buenos momentos y en los peores momentos también TE QUIERO MUCHO MI AMOR😻♥️')
+     alert('Amor eso nomas era te dije que no era gran cosa')
+     alert('TE AMO DEMAS AMOR😻♥️')
+    });
+
+const noBtn = document.querySelector('#noBtn');
+
+noBtn.addEventListener('mouseover', function () {
+    const randomX = parseInt(Math.random()*100);
+    const randomY = parseInt(Math.random()*100);
+    
+})
